a994f9c4f7138be7769552a61e5dc6367d308631
Alan Doyle <alanduvgall@gmail.com>
2022-06-27 15:11:31 +0100

    create password validation and add font-family

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("password-confirm");
+let btn = document.getElementById("submit");
+
+function validatePassword() {
+    if(password.value != confirmPassword.value) {
+            confirmPassword.setCustomValidity("Passwords do not match");
+            console.log("password.value")
+    } else {
+            confirmPassword.setCustomValidity("");
+            console.log("password.value")
+    }
+}
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
